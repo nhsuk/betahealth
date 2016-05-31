9796b7ed2a5f13060aa73ec986bf4a6ec1b78134
Dom Smith <dom@teneightfive.com>
2016-05-31 14:41:03 +0100

    Add normalize.css and reset Sets a base consistency for elements across browsers

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -1,40 +1,27 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const gulpif = require('gulp-if');
 const autoprefixer = require('gulp-autoprefixer');
+// const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
 const paths = require('../paths');
+const isProduction = process.env.NODE_ENV === 'production';
 
 // add extra sass paths
-const SASS_PATHS = [];
-
-function buildDevelopmentStyles() {
-  return gulp.src(`${paths.sourceStyles}/*.scss`)
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-      includePaths: SASS_PATHS,
-    }).on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['> 1%', 'last 2 versions', 'IE 9'],
-    }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(`${paths.outputStyles}`));
-}
-
-function buildProductionStyles() {
-  return gulp.src(`${paths.sourceStyles}/*.scss`)
-    .pipe(sass({
-      includePaths: SASS_PATHS,
-      outputStyle: 'compressed',
-    }).on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['> 1%', 'last 2 versions', 'IE 9'],
-    }))
-    .pipe(gulp.dest(`${paths.outputStyles}`));
-}
+const SASS_PATHS = [
+  `${paths.nodeModules}`,
+];
 
 gulp.task('css', () => {
-  if (process.env.NODE_ENV === 'production') {
-    return buildProductionStyles();
-  }
-  return buildDevelopmentStyles();
+  return gulp.src(`${paths.sourceStyles}/*.scss`)
+    .pipe(gulpif(!isProduction, sourcemaps.init()))
+    .pipe(sass({
+      includePaths: SASS_PATHS,
+      outputStyle: isProduction ? 'compressed' : 'expanded',
+    }).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['> 1%', 'last 2 versions', 'IE 9'],
+    }))
+    .pipe(gulpif(!isProduction, sourcemaps.write('.')))
+    .pipe(gulp.dest(`${paths.outputStyles}`));
 });
