d1d30222457cb0e5fe437da215812c8e75c4002b
Dom Smith <dom@teneightfive.com>
2016-06-17 12:22:45 +0100

    Add basic acceptance test support Add webdriverio testrunner to run acceptance tests

---
+++ test/acceptance/wdio.headless.conf.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-param-reassign */
+
+const config = require('./wdio.conf.js').config;
+
+exports.config = (function headlessConfig(globalConfig) {
+  globalConfig.capabilities = [{
+    browserName: 'phantomjs',
+  }];
+
+  globalConfig.afterTest = () => {
+    return;
+  };
+
+  return globalConfig;
+}(config));
