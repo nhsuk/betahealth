c125fb7eec639f3c7f6dd816626de4afcf03c4c2
Dom Smith <dom@teneightfive.com>
2016-07-06 17:34:44 +0100

    Add initial markdown support This adds a base class to allow the marked library to be extended with a custom renderer.
    It also allows the inclusion of markdown within a template or to render
    a file as markdown using nunjucks-markdown

--- config/express.js
+++ config/express.js
@@ -5,10 +5,13 @@ const bodyParser = require('body-parser');
 const compress = require('compression');
 const methodOverride = require('method-override');
 const nunjucks = require('nunjucks');
+const markdown = require('nunjucks-markdown');
+const marked = require('marked');
 const enforce = require('express-sslify');
 const churchill = require('churchill');
 const validator = require('express-validator');
 const csrf = require('csurf');
+const MarkdownRenderer = require('../lib/nuspeak');
 const logger = require('../lib/logger');
 const checkSecure = require('../app/middleware/check-secure');
 const locals = require('../app/middleware/locals');
@@ -21,13 +24,19 @@ const router = require('./routes');
 module.exports = (app, config) => {
   app.set('views', `${config.root}/app/views`);
   app.set('view engine', 'nunjucks');
-  const env = nunjucks.configure(`${config.root}/app/views`, {
+
+  const nunjucksEnv = nunjucks.configure(`${config.root}/app/views`, {
     autoescape: true,
     express: app,
   });
-  env.addFilter('split', (str, seperator) => {
+  nunjucksEnv.addFilter('split', (str, seperator) => {
     return str.split(seperator);
   });
+
+  marked.setOptions({
+    renderer: new MarkdownRenderer(),
+  });
+  markdown.register(nunjucksEnv, marked);
 
   if (!config.ci) {
     app.use(churchill(logger));
