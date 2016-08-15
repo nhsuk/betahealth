e0db2624fb37d7499c87f0415a1883dab231aa3c
Dom Smith <dom@teneightfive.com>
2016-08-15 11:39:21 +0100

    Add Hotjar tracking code

--- app/middleware/locals.js
+++ app/middleware/locals.js
@@ -3,6 +3,7 @@ module.exports = (config) => {
     /* eslint-disable no-param-reassign */
     res.locals.GOOGLE_ANALYTICS_TRACKING_ID = config.googleAnalyticsId;
     res.locals.WEBTRENDS_TRACKING_ID = config.webtrendsId;
+    res.locals.HOTJAR_TRACKING_ID = config.hotjarId;
     res.locals.CDN_HOST = config.staticCdn;
     res.locals.FONT_CDN_HOST = config.fontCdn;
     /* eslint-enable no-param-reassign */
