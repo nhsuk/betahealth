7e8974c444d8724bfc30c6155f3d81cbae68d644
Dom Smith <dom@teneightfive.com>
2016-10-07 12:05:42 +0100

    Add browser tests for guide layouts

---
+++ test/acceptance/pageobjects/guide-page.page.js
@@ -0,0 +1,58 @@
+const page = require('./page');
+
+const guidePage = Object.create(page, {
+  /**
+   * define elements
+   */
+  header: {
+    get: () => {
+      return browser.element('.local-header');
+    },
+  },
+  h1: {
+    get: () => {
+      return browser.element('.local-header h1');
+    },
+  },
+  navigation: {
+    get: () => {
+      return browser.element('.local-header nav.link-list--row');
+    },
+  },
+  navigationLinks: {
+    get: () => {
+      return browser.elements('.local-header nav.link-list--row a');
+    },
+  },
+  currentPage: {
+    get: () => {
+      return browser.element('.local-header nav.link-list--row .is-active');
+    },
+  },
+  pagination: {
+    get: () => {
+      return browser.element('.article-pagination');
+    },
+  },
+  nextLink: {
+    get: () => {
+      return browser.element('.article-pagination').element('*=Next page');
+    },
+  },
+  prevLink: {
+    get: () => {
+      return browser.element('.article-pagination').element('*=Previous page');
+    },
+  },
+
+  /**
+   * define or overwrite page methods
+   */
+  open: {
+    value: function open() {
+      page.open.call(this, 'conditions/type-2-diabetes');
+    },
+  },
+});
+
+module.exports = guidePage;
