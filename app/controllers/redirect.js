5ce1f7cfb9e56f78b3bc82c6ac2e949fce355436
Dom Smith <dom@teneightfive.com>
2016-10-28 16:09:23 +0100

    Redirect conditions and symptoms paths to root

---
+++ app/controllers/redirect.js
@@ -0,0 +1,7 @@
+function index(req, res) {
+  res.redirect('/');
+}
+
+module.exports = {
+  index,
+};
--- config/routes.js
+++ config/routes.js
@@ -1,12 +1,14 @@
 // eslint-disable-next-line new-cap
 const router = require('express').Router();
 const developmentOnlyController = require('../app/controllers/development-only');
+const redirectController = require('../app/controllers/redirect');
 const healthcheckController = require('../app/controllers/healthcheck');
 const contentRouter = require('./content-router');
 
 router.use('/', contentRouter);
 router.get('/:slug(elements)', developmentOnlyController.index);
 router.get('/healthcheck', healthcheckController.index);
+router.get('/*(help|symptoms|conditions)', redirectController.index);
 router.use('/*(help|symptoms|conditions)/*?', contentRouter);
 
 module.exports = router;
