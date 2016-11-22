4ee06165aa902e7f15f9347ac207592641b384fb
Dom Smith <dom@teneightfive.com>
2016-11-22 19:12:40 +0000

    Prevent content router middleware from being called twice The way the content router was loaded caused it to be loaded twice as it was being loaded for 2 paths.
    This brings the content specific middleware back into the main router
    and uses a different way to define middleware only for specific
    routes

--- app/middleware/content-api.js
+++ app/middleware/content-api.js
@@ -1,23 +1,21 @@
 const contentApi = require('../../lib/content-api');
 const parseurl = require('parseurl');
 
-module.exports = () => {
-  return (req, res, next) => {
-    const parsedUrl = parseurl.original(req);
-    const slug = parsedUrl.pathname.replace(/^\//, '') || 'index';
-    const record = contentApi.getRecord(`${slug}`);
-    let layout = slug;
+module.exports = (req, res, next) => {
+  const parsedUrl = parseurl.original(req);
+  const slug = parsedUrl.pathname.replace(/^\//, '') || 'index';
+  const record = contentApi.getRecord(`${slug}`);
+  let layout = slug;
 
-    if (record) {
-      layout = `_layouts/${record.layout}`;
-    }
+  if (record) {
+    layout = `_layouts/${record.layout}`;
+  }
 
-    record.slug = slug;
+  record.slug = slug;
 
-    /* eslint-disable no-param-reassign */
-    req.layout = layout;
-    req.pageData = record;
-    /* eslint-enable no-param-reassign */
-    next();
-  };
+  /* eslint-disable no-param-reassign */
+  req.layout = layout;
+  req.pageData = record;
+  /* eslint-enable no-param-reassign */
+  next();
 };
