97e7b05adb87b2a391f4af33d5380359d3c39448
Tyom Semonov <inspiro@gmail.com>
2017-01-03 16:33:02 +0000

    Add node Sass linter sass-lint

--- gulp/tasks/dev/lint-css.js
+++ gulp/tasks/dev/lint-css.js
@@ -1,8 +1,9 @@
-const scsslint = require('gulp-scss-lint');
+const sassLint = require('gulp-sass-lint');
 const paths = require('../../paths');
 
 module.exports = (gulp) => {
   return gulp.src(`${paths.sourceStyles}/**/*.s+(a|c)ss`)
-    .pipe(scsslint())
-    .pipe(scsslint.failReporter());
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError());
 };
