b73ccafd472338ea44d6345d7c15761ac2a8fa78
Tyom Semonov <inspiro@gmail.com>
2017-01-04 14:38:43 +0000

    Convert some of sass-lint rules to Stylelint

--- gulp/tasks/dev/lint-css.js
+++ gulp/tasks/dev/lint-css.js
@@ -1,8 +1,17 @@
 const sassLint = require('gulp-sass-lint');
+const stylelint = require('gulp-stylelint');
 const paths = require('../../paths');
 
 module.exports = (gulp) => {
   return gulp.src(`${paths.sourceStyles}/**/*.s+(a|c)ss`)
+    .pipe(stylelint({
+      syntax: 'scss',
+      debug: true,
+      reporters: [{
+        formatter: 'string',
+        console: true
+      }]
+    }))
     .pipe(sassLint())
     .pipe(sassLint.format())
     .pipe(sassLint.failOnError());
