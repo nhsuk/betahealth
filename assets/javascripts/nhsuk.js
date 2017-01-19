d056ff426d27d506a219aab1a2a2226afb755fed
Tyom Semonov <inspiro@gmail.com>
2017-01-19 19:15:56 +0000

    Add print dialogue handler

--- assets/javascripts/nhsuk.js
+++ assets/javascripts/nhsuk.js
@@ -5,6 +5,7 @@ const analytics = require('./modules/analytics');
 const labelFocus = require('./modules/label-focus');
 const labelSelect = require('./modules/label-select');
 const tabs = require('./modules/tabs');
+const print = require('./modules/print');
 
 cookieMessage('global-cookies-banner');
 
@@ -14,4 +15,5 @@ $(() => {
   labelFocus.init();
   labelSelect.init();
   tabs.init();
+  print.init();
 });
